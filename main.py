2dde18981d123f8641e4c290af1605253fc5bb6e
katyasolovii <soloviyk@gmail.com>
2024-02-19 00:05:14 +0200

    added main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+from utils import factorial, prime, stepin5, stepin2
+
+print(factorial(4))
+print(prime(5))
+print(stepin5(15625))
+print(stepin2(1022))
---
+++ utils.py
@@ -0,0 +1,33 @@
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+
+def prime(n):
+    if n <= 1:
+        return 'Число не просте'
+    if n <= 3:
+        return 'Число просте'
+    if n % 2 == 0 or n % 3 == 0:
+        return 'Число не просте'
+    return 'Число просте'
+
+
+def stepin5(n):
+    while n > 1:
+        if n % 5 != 0:
+            return 'Число не є степенем 5'
+        n /= 5
+    return 'Число є степенем 5'
+
+
+def stepin2(n):
+    if n <= 0:
+        return 'Число не є степенем 2'
+    while n != 1:
+        if n % 2 != 0:
+            return 'Число не є степенем 2'
+        n = n // 2
+    return 'Число є степенем 2'
